2163e04fb2247192805c29944ccc0f08e48b3815
robdll <dilillo.roberto@gmail.com>
2022-07-07 18:17:16 +0100

    fcc workaround

--- pages/api/shorturl.js
+++ pages/api/shorturl.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     const url = await Url.create(req.body);
 
     res.status(201).json({
-      original_url: url.original_url,
+      original_url: url.original_url || url.url,
       short_url: short_url.seq_value + 1,
     });
   } catch (error) {
